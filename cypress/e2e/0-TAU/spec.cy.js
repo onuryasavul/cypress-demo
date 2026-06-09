5609dfd423b67cb2770b75d546b1b8ccb0b33898
ONUR YASAVUL <onur.yasavul@std.yildiz.edu.tr>
2026-06-09 16:10:39 +0300

    updated Login with false user test

--- cypress/e2e/0-TAU/spec.cy.js
+++ cypress/e2e/0-TAU/spec.cy.js
@@ -1,7 +1,10 @@
-it("my first test", () => {
+it("Login with false user", () => {
   cy.visit("/");
   cy.contains("Swag Labs");
   cy.get("#user-name").type("onuryasavul");
   cy.get("#password").type("password");
-  cy.get("#login-button").click;
+  cy.get("#login-button").click();
+  cy.contains(
+    "Epic sadface: Username and password do not match any user in this service",
+  );
 });
