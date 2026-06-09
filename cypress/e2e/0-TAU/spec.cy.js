1c6ebaa053fd7e62c29365e3364b9cf4ca8f4dda
ONUR YASAVUL <onur.yasavul@std.yildiz.edu.tr>
2026-06-09 23:23:20 +0300

    added new test cases to Login Tests

--- cypress/e2e/0-TAU/spec.cy.js
+++ cypress/e2e/0-TAU/spec.cy.js
@@ -1,33 +1,49 @@
-it("Login with false user", () => {
-  cy.visit("/");
-  cy.contains("Swag Labs");
-  cy.get("#user-name").type("onuryasavul");
-  cy.get("#password").type("password");
-  cy.get("#login-button").click();
-  cy.contains(
-    "Epic sadface: Username and password do not match any user in this service",
-  );
-});
+describe("Login Tests", () => {
+  it("Load homepage", () => {
+    cy.visit("/");
+    cy.contains("Swag Labs");
+  });
 
-it("Login with correct user", () => {
-  cy.visit("/");
-  cy.contains("Swag Labs");
-  cy.get("#user-name").type("standard_user");
-  cy.get("#password").type("secret_sauce");
-  cy.get("#login-button").click();
-  cy.contains("Products");
-});
+  it("Login with false user", () => {
+    cy.visit("/");
+    cy.contains("Swag Labs");
+    cy.get("#user-name").type("onuryasavul");
+    cy.get("#password").type("password");
+    cy.get("#login-button").click();
+    cy.contains(
+      "Epic sadface: Username and password do not match any user in this service",
+    );
+  });
 
-it("Logout", () => {
-  cy.visit("/");
-  cy.contains("Swag Labs");
-  cy.get("#user-name").type("standard_user");
-  cy.get("#password").type("secret_sauce");
-  cy.get("#login-button").click();
-  cy.contains("Products");
-  cy.get("#react-burger-menu-btn").click();
-  cy.get("#logout_sidebar_link").click();
-  cy.get("#user-name");
-  cy.get("#password");
-  cy.get("#login-button");
+  it("Login with correct user", () => {
+    cy.visit("/");
+    cy.contains("Swag Labs");
+    cy.get("#user-name").type("standard_user");
+    cy.get("#password").type("secret_sauce");
+    //cy.get("#login-button").click();
+    cy.contains("Login").click();
+    cy.contains("Products");
+  });
+
+  it("Login with enter", () => {
+    cy.visit("/");
+    cy.contains("Swag Labs");
+    cy.get("#user-name").type("standard_user");
+    cy.get("#password").type("secret_sauce{enter}");
+    cy.contains("Products");
+  });
+
+  it("Logout", () => {
+    cy.visit("/");
+    cy.contains("Swag Labs");
+    cy.get("#user-name").type("standard_user");
+    cy.get("#password").type("secret_sauce");
+    cy.get("#login-button").click();
+    cy.contains("Products");
+    cy.get("#react-burger-menu-btn").click();
+    cy.get("#logout_sidebar_link").click();
+    cy.get("#user-name");
+    cy.get("#password");
+    cy.get("#login-button");
+  });
 });
